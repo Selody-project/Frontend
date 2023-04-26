01f09fa0dbc0b66df0b4adf0cb05dfd21b756eb5
Taehoya <cth6809@naver.com>
2023-04-25 22:21:27 -0500

    security analysis설정 regression testing pipeline 설정

---
+++ __test__/sample.test.js
@@ -0,0 +1,8 @@
+describe('Test Sample Test', () => {
+    it('Sample Test', async () => {
+      const expected = 2;
+      const result = 1 + 1;
+      expect(expected).toEqual(result);
+    });
+  });
+  
