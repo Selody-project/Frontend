74ecfd40d83a86e02d765bb22f89fa0755b207aa
김영훈 <joseph0926@naver.com>
2023-05-07 11:05:07 +0900

    #3 sign-fix

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { Outlet, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
 
 const Root = () => {
-  const { isLoading, user } = useSelector((state) => state.user);
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (!user) {
-      navigate("/landing");
-    }
-  }, [user]);
+  const { userLoading, user } = useSelector((state) => state.user);
+
+  if (userLoading) {
+    return <p>Loading,,,</p>;
+  }
+  if (!user) {
+    return <Navigate to="/landing" />;
+  }
 
   return (
     <div>
