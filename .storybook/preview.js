d01213bac28d64932c5e79a7dff51b1480099f3b
uiseop <bono521@naver.com>
2023-05-22 16:14:20 +0900

    chore: styled-componets Resetcss 적용 및 storybook 적용 (#29)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,10 @@
 /** @type { import('@storybook/react').Preview } */
-const preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-  },
-};
 
-export default preview;
+import { withThemeFromJSXProvider } from "@storybook/addon-styling";
+import { GlobalStyles } from "../src/styles/GlobalStyles";
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    GlobalStyles,
+  }),
+];
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Modal from 'react-modal';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Modal from "react-modal";
+import App from "./App";
+import "./index.css";
 
-import { Provider } from 'react-redux';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { store } from './store/store';
+import { Provider } from "react-redux";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { store } from "./store/store";
+import GlobalStyles from "./styles/GlobalStyles";
 
 if (Modal && Modal.setAppElement) {
-  Modal.setAppElement('#root');
+  Modal.setAppElement("#root");
 }
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <GoogleOAuthProvider clientId="379597382111-vo2ht0r8a3d0ais7v12q7777lu48al1a.apps.googleusercontent.com">
+      <GlobalStyles />
       <App />
     </GoogleOAuthProvider>
   </Provider>
