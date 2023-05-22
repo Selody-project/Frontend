10220e5e4528e4b8048a44459549e97dd1fc2366
uiseop <bono521@naver.com>
2023-05-22 16:20:33 +0900

    chore: default theme 추가, 다크모드를 대비하여 theme 추가 및 storybook 적용 (#29)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,9 +2,16 @@
 
 import { withThemeFromJSXProvider } from "@storybook/addon-styling";
 import { GlobalStyles } from "../src/styles/GlobalStyles";
+import { lightTheme } from "../src/styles/theme";
+import { ThemeProvider } from "styled-components";
 
 export const decorators = [
   withThemeFromJSXProvider({
+    themes: {
+      light: lightTheme,
+    },
+    defaultTheme: "light",
+    Provider: ThemeProvider,
     GlobalStyles,
   }),
 ];
---
+++ src/styles/theme.js
@@ -0,0 +1,46 @@
+export const lightTheme = {
+  colors: {
+    background: "#F6F9FC",
+    backgroundInverse: "#7A8997",
+    positive: "#E1FFD4",
+    negative: "#FEDED2",
+    primary: "#FF4785",
+    secondary: "#1EA7FD",
+    tertiary: "#DDDDDD",
+    text: "#222222",
+  },
+  spacing: {
+    padding: {
+      small: 10,
+      medium: 20,
+      large: 30,
+    },
+    borderRadius: {
+      small: 5,
+      default: 10,
+    },
+  },
+  typography: {
+    type: {
+      primary: '"Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif',
+      code: '"SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace',
+    },
+    weight: {
+      regular: "400",
+      bold: "700",
+      extrabold: "800",
+      black: "900",
+    },
+    size: {
+      s1: 12,
+      s2: 14,
+      s3: 16,
+      m1: 20,
+      m2: 24,
+      m3: 28,
+      l1: 32,
+      l2: 40,
+      l3: 48,
+    },
+  },
+};
