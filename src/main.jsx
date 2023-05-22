7c2fceec6364a6d675d79af771015510d0d27cb2
uiseop <bono521@naver.com>
2023-05-22 16:35:10 +0900

    remove: index.css 제거 (#33)

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Modal from "react-modal";
 import App from "./App";
-import "./index.css";
 
 import { Provider } from "react-redux";
 import { GoogleOAuthProvider } from "@react-oauth/google";
