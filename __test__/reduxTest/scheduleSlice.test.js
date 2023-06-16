120549b32c9b30e0dc4a44de8fbcfedbbd3913dc
김영훈 <joseph0926@naver.com>
2023-06-16 11:57:36 +0900

    test: 개인일정-thunk (#51)

--- __test__/reduxTest/scheduleSlice.test.js
+++ __test__/reduxTest/scheduleSlice.test.js
@@ -1,12 +1,5 @@
 import configureMockStore from "redux-mock-store";
 import thunk from "redux-thunk";
-import { toast } from "react-toastify";
-import {
-	saveSchedule,
-	currentMonthFn,
-	currentYearFn,
-	setId,
-} from "../../src/features/schedule/schedule-slice.js";
 import { server } from "../__mocks__/msw/server.js";
 import { createSchedule } from "../../src/features/schedule/schedule-service.js";
 
@@ -61,17 +54,16 @@ describe("schedule slice", () => {
 			message: "Successfully create user schedule",
 		});
 
-		const fetchResponse = await store.dispatch(createSchedule()).unwrap();
+		const fetchResponse = await store
+			.dispatch(createSchedule(mockPayload.schedule))
+			.unwrap();
 
 		const actions = store.getActions();
 		expect(actions[0].type).toEqual("schedule/createSchedule/pending");
 		expect(actions[1].type).toEqual("schedule/getSchedule/pending");
 		expect(actions[2].type).toEqual("schedule/createSchedule/fulfilled");
-		expect(actions[3].type).toEqual("schedule/getSchedule/fulfilled");
-		expect(fetchResponse.recurrenceSchedule[0].content).toEqual(
-			"test-content11",
-		);
-		expect(toast.success).toHaveBeenCalledWith("일정 추가에 성공하셨습니다!");
+		expect(actions[3].type).toEqual("schedule/createSchedule/pending");
+		expect(actions[4].type).toEqual("schedule/getSchedule/fulfilled");
 	});
 
 	it("should handle createSchedule success without recurrence", async () => {
@@ -93,17 +85,15 @@ describe("schedule slice", () => {
 		expect(createResponse).toEqual({
 			message: "Successfully create user schedule",
 		});
-
-		const fetchResponse = await store.dispatch(createSchedule()).unwrap();
+		const fetchResponse = await store
+			.dispatch(createSchedule(mockPayload.schedule))
+			.unwrap();
 
 		const actions = store.getActions();
 		expect(actions[0].type).toEqual("schedule/createSchedule/pending");
 		expect(actions[1].type).toEqual("schedule/getSchedule/pending");
 		expect(actions[2].type).toEqual("schedule/createSchedule/fulfilled");
-		expect(actions[3].type).toEqual("schedule/getSchedule/fulfilled");
-		expect(fetchResponse.recurrenceSchedule[0].content).toEqual(
-			"test-content11",
-		);
-		expect(toast.success).toHaveBeenCalledWith("일정 추가에 성공하셨습니다!");
+		expect(actions[3].type).toEqual("schedule/createSchedule/pending");
+		expect(actions[4].type).toEqual("schedule/getSchedule/fulfilled");
 	});
 });
