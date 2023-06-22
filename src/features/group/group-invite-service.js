b3faca59423a76060b478751578ebd7cb0168d35
joseph0926 <joseph0926@naver.com>
2023-06-22 22:30:05 +0900

    feat: 그룹초대 (#67)

---
+++ src/features/group/group-invite-service.js
@@ -0,0 +1,65 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import customFetch from "@/components/Base/BaseAxios.js";
+
+export const createInviteLink = createAsyncThunk(
+	"group/createInviteLink",
+	async (groupId, thunkAPI) => {
+		try {
+			const response = await customFetch.post(
+				`/back/api/group/${groupId}/invite-link`,
+			);
+			if (response.status !== 200) {
+				throw response.data;
+			}
+			return response.data;
+		} catch (error) {
+			if (error.response) {
+				return thunkAPI.rejectWithValue(error.response.data);
+			} else {
+				return thunkAPI.rejectWithValue(error.message);
+			}
+		}
+	},
+);
+
+export const getInvitation = createAsyncThunk(
+	"group/getInvitation",
+	async (inviteCode, thunkAPI) => {
+		try {
+			const response = await customFetch.get(
+				`/back/api/group/invite-link/${inviteCode}`,
+			);
+			if (response.status !== 200) {
+				throw response.data;
+			}
+			return response.data;
+		} catch (error) {
+			if (error.response) {
+				return thunkAPI.rejectWithValue(error.response.data);
+			} else {
+				return thunkAPI.rejectWithValue(error.message);
+			}
+		}
+	},
+);
+
+export const groupJoin = createAsyncThunk(
+	"group/groupJoin",
+	async (inviteCode, thunkAPI) => {
+		try {
+			const response = await customFetch.get(
+				`/back/api/group/join/${inviteCode}`,
+			);
+			if (response.status !== 200) {
+				throw response.data;
+			}
+			return response.data;
+		} catch (error) {
+			if (error.response) {
+				return thunkAPI.rejectWithValue(error.response.data);
+			} else {
+				return thunkAPI.rejectWithValue(error.message);
+			}
+		}
+	},
+);
---
+++ src/features/group/group-invite-slice.js
@@ -0,0 +1,56 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {
+	createInviteLink,
+	getInvitation,
+	groupJoin,
+} from "./group-invite-service.js";
+
+const initialState = {
+	inviteCode: "",
+	inviteExp: null,
+};
+
+const groupSlice = createSlice({
+	name: "groupInvite",
+	initialState,
+	reducers: {},
+	extraReducers: (bulider) => {
+		bulider
+			.addCase(createInviteLink.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(createInviteLink.fulfilled, (state, { payload }) => {
+				state.isLoading = false;
+				state.inviteCode = payload.inviteCode;
+				state.inviteExp = payload.exp;
+				toast.success("그룹초대 코드 생성에 성공하셨습니다!");
+			})
+			.addCase(createInviteLink.rejected, (state, { payload }) => {
+				state.isLoading = false;
+				console.log(payload);
+			})
+			// .addCase(getInvitation.pending, (state) => {
+			// 	state.isLoading = true;
+			// })
+			// .addCase(getInvitation.fulfilled, (state, { payload }) => {
+			// 	state.isLoading = false;
+			// })
+			// .addCase(getInvitation.rejected, (state, { payload }) => {
+			// 	state.isLoading = false;
+			// 	console.log(payload);
+			// })
+			.addCase(groupJoin.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(groupJoin.fulfilled, (state, { payload }) => {
+				state.isLoading = false;
+				toast.success("그룹에 참여하는데 성공하였습니다.");
+			})
+			.addCase(groupJoin.rejected, (state, { payload }) => {
+				state.isLoading = false;
+				console.log(payload);
+			});
+	},
+});
+
+export default groupSlice.reducer;
