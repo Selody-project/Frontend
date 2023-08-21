97599f2b81dff0193973e6937a947a745cd1c071
kimyoungyin <yin199859@gmail.com>
2023-08-21 14:05:31 +0900

    feat: GroupHeader 구현

--- src/components/Header/GroupHeader/GroupHeader.jsx
+++ src/components/Header/GroupHeader/GroupHeader.jsx
@@ -1,42 +1,26 @@
 import React from "react";
-import { useDispatch } from "react-redux";
 import { NavLink, useLocation } from "react-router-dom";
 
-import { openModal } from "@/features/ui/ui-slice.js";
-
-import { Wrapper, MenuWrapper, GroupCreateBtn } from "./GroupHeader.styles";
+import { GroupHeaderNav, GroupHeaderUl } from "./GroupHeader.styles";
 
 const GroupHeader = () => {
-	const dispatch = useDispatch();
 	const location = useLocation();
-
-	const getPageTitle = () => {
-		switch (location.pathname) {
-			case "/":
-				return "개인 일정";
-			default:
-				return "공유 일정";
-		}
-	};
-
+	const isPersonal = location.pathname === "/";
 	return (
-		<Wrapper>
-			<MenuWrapper>
-				<h1>{getPageTitle()}</h1>
-				<ul>
-					<NavLink to="/share">홈</NavLink>
-					<NavLink to="/">개인 일정</NavLink>
-				</ul>
-			</MenuWrapper>
-			<GroupCreateBtn>
-				<button
-					type="button"
-					onClick={() => dispatch(openModal({ type: "SHARE_PAGE_CREATE" }))}
-				>
-					공유 페이지 생성
-				</button>
-			</GroupCreateBtn>
-		</Wrapper>
+		<GroupHeaderNav>
+			<GroupHeaderUl>
+				<li>
+					<NavLink to="/" className={`${isPersonal ? "activated" : ""}`}>
+						개인 일정
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/share" className={`${!isPersonal ? "activated" : ""}`}>
+						그룹 일정
+					</NavLink>
+				</li>
+			</GroupHeaderUl>
+		</GroupHeaderNav>
 	);
 };
 
