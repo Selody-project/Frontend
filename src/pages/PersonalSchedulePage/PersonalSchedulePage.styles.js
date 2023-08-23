ccaa4d1dcaa49422944e8d23089a1a54f83ecfc8
kimyoungyin <yin199859@gmail.com>
2023-08-23 17:36:31 +0900

    issue #124 refactor: change MainContainer to LayoutMain and move directory styled-component가 jsx파일 내에 같이 잘못된 명명법으로 선언되어 있어 폴더 생성 후 함꼐 이동

---
+++ src/pages/PersonalSchedulePage/PersonalSchedulePage.styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+// 나중에 컴포넌트 추가할 수도 있으므로 export const로
+export const LayoutMain = styled.main`
+	display: flex;
+	justify-content: center;
+	gap: 13px;
+	padding: 50px 60px 0;
+	font-family: "Inter", sans-serif;
+`;
