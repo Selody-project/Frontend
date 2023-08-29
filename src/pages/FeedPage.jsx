f697ab9770ac724b9b6506ccb56c9a4d6a824d69
sikkzz <sling0623@gmail.com>
2023-08-29 18:24:58 +0900

    #117 refactor: feed page header delete

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -4,12 +4,10 @@ import React from "react";
 // import FeedHeader from "@/components/Header/FeedHeader/FeedHeader";
 
 import FeedLanding from "@/components/Feed/FeedLanding/FeedLanding";
-import Header from "@/components/Header/Header/Header";
 
 const FeedPage = () => {
 	return (
 		<>
-			<Header />
 			<FeedLanding />
 			{/* <FeedHeader /> */}
 			{/* <FeedMain /> */}
