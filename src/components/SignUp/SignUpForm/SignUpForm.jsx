ac0a0afd3f71a5c2a071ed8eb652340cbb5754e2
2Junsu <ijh1205@naver.com>
2023-08-30 20:25:05 +0900

    #126 feat: Add input focus whhen something is wrong

--- src/components/SignUp/SignUpForm/SignUpForm.jsx
+++ src/components/SignUp/SignUpForm/SignUpForm.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+/* eslint-disable no-return-assign */
+import React, { useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
 
@@ -15,6 +16,8 @@ import {
 
 const SignUpForm = () => {
 	const dispatchFn = useDispatch();
+
+	const signUpRef = useRef([]);
 
 	const [formValue, setFormValue] = useState({
 		email: "",
@@ -42,17 +45,17 @@ const SignUpForm = () => {
 		setFormValue(changed);
 	};
 
-	const checkDuplication = async (type) => {
+	const isEmailValid = () => {
 		const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
-		if (type === "email" && !emailRegex.test(email)) {
-			toast.error("올바른 이메일 형식이 아닙니다. 다시 입력해주세요.");
-		}
+		return emailRegex.test(email);
+	};
 
+	const checkDuplication = async (type) => {
 		const koreanType = type === "email" ? "이메일" : "닉네임";
-		const [targetKey, targetValue] =
+		const [targetKey, targetValue, targetRefIdx] =
 			type === "email"
-				? ["checkedEmail", email]
-				: ["checkedNickname", nickname];
+				? ["checkedEmail", email, 0]
+				: ["checkedNickname", nickname, 1];
 		const response = await dispatchFn(
 			validateDuplication({ type, targetValue }),
 		);
@@ -61,6 +64,7 @@ const SignUpForm = () => {
 			toast.success(`사용 가능한 ${koreanType}입니다.`);
 			setFormValue({ ...formValue, [targetKey]: targetValue });
 		} else {
+			signUpRef.current[targetRefIdx].focus();
 			toast.error(`이미 존재하는 ${koreanType}입니다.`);
 		}
 	};
@@ -75,11 +79,13 @@ const SignUpForm = () => {
 
 	const validate = () => {
 		if (password !== passwordCheck) {
+			signUpRef.current[3].focus();
 			toast.error("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
 			return false;
 		}
 
 		if (password.length < 10) {
+			signUpRef.current[2].focus();
 			toast.error("비밀번호는 10자 이상이어야 합니다.");
 			return false;
 		}
@@ -101,6 +107,7 @@ const SignUpForm = () => {
 				<label htmlFor="email">아이디 (이메일)</label>
 				<InputInnerDiv>
 					<Input
+						ref={(el) => (signUpRef.current[0] = el)}
 						data-testid="email-input"
 						type="email"
 						id="email"
@@ -112,8 +119,8 @@ const SignUpForm = () => {
 					<DuplicateCheckButton
 						data-testid="email-duplicate-check-button"
 						type="button"
-						disabled={email === ""}
-						isActive={email !== ""}
+						disabled={!isEmailValid()}
+						isActive={isEmailValid()}
 						onClick={() => checkDuplication("email")}
 					>
 						중복확인
@@ -124,6 +131,7 @@ const SignUpForm = () => {
 				<label htmlFor="nickname">어떤 이름을 사용하시겠어요?</label>
 				<InputInnerDiv>
 					<Input
+						ref={(el) => (signUpRef.current[1] = el)}
 						data-testid="nickname-input"
 						id="nickname"
 						name="nickname"
@@ -146,6 +154,7 @@ const SignUpForm = () => {
 				<label htmlFor="password">비밀번호</label>
 				<InputInnerDiv>
 					<Input
+						ref={(el) => (signUpRef.current[2] = el)}
 						data-testid="password-input"
 						type="password"
 						id="password"
@@ -160,6 +169,7 @@ const SignUpForm = () => {
 				<label htmlFor="passwordCheck">비밀번호 확인</label>
 				<InputInnerDiv>
 					<Input
+						ref={(el) => (signUpRef.current[3] = el)}
 						data-testid="password-check-input"
 						type="password"
 						id="passwordCheck"
