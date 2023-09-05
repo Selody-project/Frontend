c1fc05a2ef6ffad8800bc65bdb59b6a3244bb2f3
sikkzz <sling0623@gmail.com>
2023-09-05 18:59:18 +0900

    issue #115 refactor: conflict resolve

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 import FeedLanding from "@/components/Feed/FeedLanding/FeedLanding";
 
-const FeedPage = () => {
-	return <FeedLanding />;
-};
+const FeedPage = () => <FeedLanding />;
 
 export default FeedPage;
