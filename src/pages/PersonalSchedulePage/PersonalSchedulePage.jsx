6b0c4603d99b523afec2f17001564b0c8c2ed9b7
kimyoungyin <yin199859@gmail.com>
2023-09-22 16:23:05 +0900

    issue #139 refactor: fix component name from PersonalTodoList to ScheduleItemList

--- src/pages/PersonalSchedulePage/PersonalSchedulePage.jsx
+++ src/pages/PersonalSchedulePage/PersonalSchedulePage.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
 import CalendarContainer from "@/components/Common/CalendarContainer/CalendarContainer";
+import ScheduleItemList from "@/components/ScheduleItemList/ScheduleItemList";
 import { SCHEDULE_TYPE } from "@/constants/calendarConstants";
 
 import { LayoutMain } from "./PersonalSchedulePage.styles";
-import PersonalTodoList from "../../components/ScheduleItemList/ScheduleItemList";
 
 const PersonalSchedulePage = () => {
 	return (
 		<LayoutMain>
 			<CalendarContainer type={SCHEDULE_TYPE.PERSONAL} />
-			<PersonalTodoList />
+			<ScheduleItemList />
 		</LayoutMain>
 	);
 };
