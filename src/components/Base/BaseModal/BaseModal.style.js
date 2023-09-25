db04fc972de8db5b01ff65e1e90d5eb9b82c7534
kimyoungyin <yin199859@gmail.com>
2023-09-25 10:19:41 +0900

    issue #139 feat: set BaseModal title layout

--- src/components/Base/BaseModal/BaseModal.style.js
+++ src/components/Base/BaseModal/BaseModal.style.js
@@ -43,3 +43,20 @@ export const IconButton = styled.button`
 	top: 0;
 	right: 0;
 `;
+// export const ModalHeaderDiv = styled.div`
+// 	display: flex;
+// 	justify-content: space-between;
+// 	align-items: center;
+// 	font-size: 20px;
+// 	font-weight: 600;
+// 	line-height: normal;
+// 	position: relative;
+// 	width: 100%;
+// 	padding: 0 20px;
+// 	margin-top: 34px;
+// 	margin-bottom: 24px;
+// 	color: ${({ theme }) => theme.colors.text_01};
+// 	& > svg {
+// 		cursor: pointer;
+// 	}
+// `;
