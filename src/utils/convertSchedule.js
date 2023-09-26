e96bec4f9d3c1d47f0a8392b82557e8674d176bb
kimyoungyin <yin199859@gmail.com>
2023-09-26 10:51:30 +0900

    issue #139 fix: set null in byweekday when it's length is 0

--- src/utils/convertSchedule.js
+++ src/utils/convertSchedule.js
@@ -18,32 +18,35 @@ export const convertScheduleFormValueToData = ({
 		: convertToUTC(endDate, endTime);
 	const untileDateTime = until ? convertToUTC(until, "00:00") : null;
 
-	byweekday = byweekday
-		.map((weekNum) => {
-			if (weekNum === 0) {
-				return "SU";
-			}
-			if (weekNum === 1) {
-				return "MO";
-			}
-			if (weekNum === 2) {
-				return "TU";
-			}
-			if (weekNum === 3) {
-				return "WE";
-			}
-			if (weekNum === 4) {
-				return "TH";
-			}
-			if (weekNum === 5) {
-				return "FR";
-			}
-			if (weekNum === 6) {
-				return "SA";
-			}
-			throw new Error("존재하지 않는 요일입니다.");
-		})
-		.toString();
+	byweekday =
+		byweekday.length > 0
+			? byweekday
+					.map((weekNum) => {
+						if (weekNum === 0) {
+							return "SU";
+						}
+						if (weekNum === 1) {
+							return "MO";
+						}
+						if (weekNum === 2) {
+							return "TU";
+						}
+						if (weekNum === 3) {
+							return "WE";
+						}
+						if (weekNum === 4) {
+							return "TH";
+						}
+						if (weekNum === 5) {
+							return "FR";
+						}
+						if (weekNum === 6) {
+							return "SA";
+						}
+						throw new Error("존재하지 않는 요일입니다.");
+					})
+					.toString()
+			: null;
 
 	return {
 		title,
