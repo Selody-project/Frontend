4a90a6c2a334b3c387c2cab04f9d73673734ce08
kimyoungyin <yin199859@gmail.com>
2023-10-05 16:57:46 +0900

    issue #139 refactor: minimize updateSchedule thunk with convertSchedule utils

--- src/features/schedule/schedule-service.js
+++ src/features/schedule/schedule-service.js
@@ -4,7 +4,6 @@ import customFetch from "@/components/UI/BaseAxios.js";
 import { VIEW_TYPE } from "@/constants/calendarConstants";
 import { getFirstDateOfWeek } from "@/utils/calendarUtils";
 import { convertScheduleFormValueToData } from "@/utils/convertSchedule";
-import convertToUTC from "@/utils/convertToUTC.js";
 
 export const getTodaySchedules = createAsyncThunk(
 	"schedule/getTodaySchedules",
@@ -162,29 +161,11 @@ export const deleteSchedule = createAsyncThunk(
 export const updateSchedule = createAsyncThunk(
 	"schedule/updateSchedule",
 	async ({ schedule, id }, thunkAPI) => {
-		const {
-			title,
-			details,
-			startDate,
-			endDate,
-			startTime,
-			endTime,
-			repeat,
-			// notification,
-		} = schedule;
-
-		const startDateTime = convertToUTC(startDate, startTime);
-		const endDateTime = convertToUTC(endDate, endTime);
-
 		try {
-			const response = await customFetch.put(`/api/user/calendar/${id}`, {
-				title,
-				content: details,
-				startDateTime,
-				endDateTime,
-				freq: repeat,
-				recurrence: 0,
-			});
+			const response = await customFetch.put(
+				`/api/user/calendar/${id}`,
+				convertScheduleFormValueToData(schedule),
+			);
 			if (response.status !== 201) {
 				throw response.data;
 			}
