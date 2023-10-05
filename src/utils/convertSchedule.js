563648644fde78464f46557110906b314f7cf0e1
kimyoungyin <yin199859@gmail.com>
2023-10-05 11:19:23 +0900

    issue #139 refactor: change schedule form value and data to array

--- src/utils/convertSchedule.js
+++ src/utils/convertSchedule.js
@@ -23,32 +23,30 @@ export const convertScheduleFormValueToData = ({
 
 	byweekday =
 		byweekday.length > 0
-			? byweekday
-					.map((weekNum) => {
-						if (weekNum === 0) {
-							return "SU";
-						}
-						if (weekNum === 1) {
-							return "MO";
-						}
-						if (weekNum === 2) {
-							return "TU";
-						}
-						if (weekNum === 3) {
-							return "WE";
-						}
-						if (weekNum === 4) {
-							return "TH";
-						}
-						if (weekNum === 5) {
-							return "FR";
-						}
-						if (weekNum === 6) {
-							return "SA";
-						}
-						throw new Error("존재하지 않는 요일입니다.");
-					})
-					.toString()
+			? byweekday.map((weekNum) => {
+					if (weekNum === 0) {
+						return "SU";
+					}
+					if (weekNum === 1) {
+						return "MO";
+					}
+					if (weekNum === 2) {
+						return "TU";
+					}
+					if (weekNum === 3) {
+						return "WE";
+					}
+					if (weekNum === 4) {
+						return "TH";
+					}
+					if (weekNum === 5) {
+						return "FR";
+					}
+					if (weekNum === 6) {
+						return "SA";
+					}
+					throw new Error("존재하지 않는 요일입니다.");
+			  })
 			: null;
 
 	return {
