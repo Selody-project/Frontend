915ba54e73042e4a9570486a182592f0f239077e
kimyoungyin <yin199859@gmail.com>
2023-10-17 17:50:41 +0900

    issue #139 test: test tab switching

--- __test__/pagesTest/PersonalSchedulePage.test.jsx
+++ __test__/pagesTest/PersonalSchedulePage.test.jsx
@@ -3,11 +3,13 @@
  */
 import React from "react";
 
+import { userEvent } from "@storybook/testing-library";
 import { screen } from "@testing-library/react";
 
 import { render } from "../../jest.setup.js";
 import "@testing-library/jest-dom";
 import PersonalSchedulePage from "../../src/pages/PersonalSchedulePage/PersonalSchedulePage.jsx";
+import lightTheme from "../../src/styles/theme.js";
 
 jest.mock("@fullcalendar/react", () => () => (
 	<div data-testid="mock-fullcalendar" />
@@ -32,5 +34,43 @@ describe("PersonalSchedulePage", () => {
 		render(<PersonalSchedulePage />);
 		expect(screen.getByTestId("calendar-container")).toBeInTheDocument();
 		expect(screen.getByTestId("personal-todo-list")).toBeInTheDocument();
+	});
+
+	it("render todaySchedules tab when first rendered", () => {
+		render(<PersonalSchedulePage />);
+
+		const element = screen.getByRole("button", { name: "오늘 일정" });
+		expect(element).not.toBeNull();
+	});
+
+	it("render schedulesForTheWeek tab when click '예정' button", () => {
+		render(<PersonalSchedulePage />);
+
+		const schedulesForTheWeekTab = screen.getByRole("button", { name: "예정" });
+		userEvent.click(schedulesForTheWeekTab);
+
+		const todaySchedulesTab = screen.getByRole("button", { name: "오늘 일정" });
+		expect(todaySchedulesTab).toHaveStyle(
+			`background-color: ${lightTheme.colors.white}`,
+		);
+		expect(schedulesForTheWeekTab).toHaveStyle(
+			`background-color: ${lightTheme.colors.primary}`,
+		);
+	});
+
+	it("render todaySchedules tab when click '오늘 일정' button after click '예정' button", () => {
+		render(<PersonalSchedulePage />);
+
+		const schedulesForTheWeekTab = screen.getByRole("button", { name: "예정" });
+		const todaySchedulesTab = screen.getByRole("button", { name: "오늘 일정" });
+		userEvent.click(schedulesForTheWeekTab);
+		userEvent.click(todaySchedulesTab);
+
+		expect(todaySchedulesTab).toHaveStyle(
+			`background-color: ${lightTheme.colors.primary}`,
+		);
+		expect(schedulesForTheWeekTab).toHaveStyle(
+			`background-color: ${lightTheme.colors.white}`,
+		);
 	});
 });
