033347c0ab017178af59f99c4b5c0b688855711c
sikkzz <sling0623@gmail.com>
2023-10-22 03:54:34 +0900

    issue #131 feat: inqueryReqeustUserGroup, refactor: api common thunk refactoring

--- src/features/user/user-service.js
+++ src/features/user/user-service.js
@@ -2,20 +2,40 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 import customFetch from "@/components/Base/BaseAxios";
 
+const commonThunk = async (params, successCode, thunkAPI) => {
+	try {
+		const response = await customFetch.request(params);
+		if (response.status !== successCode) {
+			throw response.data;
+		}
+		return response.data;
+	} catch (error) {
+		return thunkAPI.rejectWithValue(error.message);
+	}
+};
+
 export const inqueryUserGroup = createAsyncThunk(
 	"user/inqueryUserGroup",
 	async (thunkAPI) => {
-		try {
-			const response = await customFetch.get("/api/user/group");
-			if (response.status !== 200) {
-				throw response.data;
-			}
-			return response.data;
-		} catch (error) {
-			if (error.response) {
-				return thunkAPI.rejectWithValue(error.response.data);
-			}
-			return thunkAPI.rejectWithValue(error.message);
-		}
+		const data = await commonThunk(
+			{ method: "GET", url: "/api/user/group" },
+			200,
+			thunkAPI,
+		);
+
+		return data;
+	},
+);
+
+export const inqueryRequestUserGroup = createAsyncThunk(
+	"user/inqueryRequestUserGroup",
+	async (thunkAPI) => {
+		const data = await commonThunk(
+			{ method: "GET", url: "/api/user/group/pending" },
+			200,
+			thunkAPI,
+		);
+
+		return data;
 	},
 );
