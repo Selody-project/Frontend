76d2dd294fa1a2d5a5e8a5b712e786e3c6b89141
kimyoungyin <yin199859@gmail.com>
2023-11-07 11:16:40 +0900

    issue #139 fix: remove overlapping try...catch statement in schedule-service

--- src/features/schedule/schedule-service.js
+++ src/features/schedule/schedule-service.js
@@ -8,69 +8,54 @@ import { convertScheduleFormValueToData } from "@/utils/convertSchedule";
 export const getTodaySchedules = createAsyncThunk(
 	"schedule/getTodaySchedules",
 	async (_, thunkAPI) => {
-		try {
-			const today = new Date();
-			today.setHours(0, 0, 0, 0);
-			const startDateTime = today.toISOString();
-			const endDateTime = new Date(
-				today.setDate(today.getDate() + 1),
-			).toISOString();
+		const today = new Date();
+		today.setHours(0, 0, 0, 0);
+		const startDateTime = today.toISOString();
+		const endDateTime = new Date(
+			today.setDate(today.getDate() + 1),
+		).toISOString();
 
-			const data = await commonThunk(
-				{
-					method: "GET",
-					url: "/api/user/calendar",
-					params: {
-						startDateTime,
-						endDateTime,
-					},
-					successCode: 200,
+		const data = await commonThunk(
+			{
+				method: "GET",
+				url: "/api/user/calendar",
+				params: {
+					startDateTime,
+					endDateTime,
 				},
-				thunkAPI,
-			);
-			return data;
-		} catch (error) {
-			if (error.response) {
-				return thunkAPI.rejectWithValue(error.response.data);
-			}
-			return thunkAPI.rejectWithValue(error.message);
-		}
+				successCode: 200,
+			},
+			thunkAPI,
+		);
+		return data;
 	},
 );
 
 export const getSchedulesForTheWeek = createAsyncThunk(
 	"schedule/getSchedulesForTheWeek",
 	async (_, thunkAPI) => {
-		try {
-			const today = new Date();
-			today.setHours(0, 0, 0, 0);
-			const startDateTime = new Date(
-				today.setDate(today.getDate() + 1),
-			).toISOString();
-			const endDateTime = new Date(
-				today.setDate(today.getDate() + 6),
-			).toISOString();
+		const today = new Date();
+		today.setHours(0, 0, 0, 0);
+		const startDateTime = new Date(
+			today.setDate(today.getDate() + 1),
+		).toISOString();
+		const endDateTime = new Date(
+			today.setDate(today.getDate() + 6),
+		).toISOString();
 
-			const data = await commonThunk(
-				{
-					method: "GET",
-					url: "/api/user/calendar",
-					params: {
-						startDateTime,
-						endDateTime,
-					},
-					successCode: 200,
+		const data = await commonThunk(
+			{
+				method: "GET",
+				url: "/api/user/calendar",
+				params: {
+					startDateTime,
+					endDateTime,
 				},
-				thunkAPI,
-			);
-
-			return data;
-		} catch (error) {
-			if (error.response) {
-				return thunkAPI.rejectWithValue(error.response.data);
-			}
-			return thunkAPI.rejectWithValue(error.message);
-		}
+				successCode: 200,
+			},
+			thunkAPI,
+		);
+		return data;
 	},
 );
 
@@ -110,91 +95,61 @@ export const getSchedulesSummary = createAsyncThunk(
 				"isGroup일 때는 groupId가 필수이며, isGroup이 아닐 때는 groupId가 필요 없습니다.",
 			);
 
-		try {
-			const data = await commonThunk(
-				{
-					method: "GET",
-					url: `/api/${
-						!isGroup ? "user" : `group/${groupId}`
-					}/calendar/summary`,
-					params: {
-						startDateTime,
-						endDateTime,
-					},
-					successCode: 200,
+		const data = await commonThunk(
+			{
+				method: "GET",
+				url: `/api/${!isGroup ? "user" : `group/${groupId}`}/calendar/summary`,
+				params: {
+					startDateTime,
+					endDateTime,
 				},
-				thunkAPI,
-			);
-			return data;
-		} catch (error) {
-			if (error.response) {
-				return thunkAPI.rejectWithValue(error.response.data);
-			}
-			return thunkAPI.rejectWithValue(error.message);
-		}
+				successCode: 200,
+			},
+			thunkAPI,
+		);
+		return data;
 	},
 );
 
 export const createSchedule = createAsyncThunk(
 	"schedule/createSchedule",
 	async (schedule, thunkAPI) => {
-		try {
-			const data = await commonThunk(
-				{
-					method: "POST",
-					url: `/api/user/calendar`,
-					data: convertScheduleFormValueToData(schedule),
-					successCode: 201,
-				},
-				thunkAPI,
-			);
-			return data;
-		} catch (error) {
-			if (error.response) {
-				return thunkAPI.rejectWithValue(error.response.data);
-			}
-			return thunkAPI.rejectWithValue(error.message);
-		}
+		const data = await commonThunk(
+			{
+				method: "POST",
+				url: `/api/user/calendar`,
+				data: convertScheduleFormValueToData(schedule),
+				successCode: 201,
+			},
+			thunkAPI,
+		);
+		return data;
 	},
 );
 
 export const deleteSchedule = createAsyncThunk(
 	"schedule/deleteSchedule",
 	async (id, thunkAPI) => {
-		try {
-			const data = await commonThunk(
-				{ method: "DELETE", url: `/api/user/calendar/${id}`, successCode: 204 },
-				thunkAPI,
-			);
-			return data;
-		} catch (error) {
-			if (error.response) {
-				return thunkAPI.rejectWithValue(error.response.data);
-			}
-			return thunkAPI.rejectWithValue(error.message);
-		}
+		const data = await commonThunk(
+			{ method: "DELETE", url: `/api/user/calendar/${id}`, successCode: 204 },
+			thunkAPI,
+		);
+		return data;
 	},
 );
 
 export const updateSchedule = createAsyncThunk(
 	"schedule/updateSchedule",
 	async ({ schedule, id }, thunkAPI) => {
-		try {
-			const data = await commonThunk(
-				{
-					method: "PUT",
-					url: `/api/user/calendar/${id}`,
-					data: convertScheduleFormValueToData(schedule),
-					successCode: 201,
-				},
-				thunkAPI,
-			);
-			return data;
-		} catch (error) {
-			if (error.response) {
-				return thunkAPI.rejectWithValue(error.response.data);
-			}
-			return thunkAPI.rejectWithValue(error.message);
-		}
+		const data = await commonThunk(
+			{
+				method: "PUT",
+				url: `/api/user/calendar/${id}`,
+				data: convertScheduleFormValueToData(schedule),
+				successCode: 201,
+			},
+			thunkAPI,
+		);
+		return data;
 	},
 );
