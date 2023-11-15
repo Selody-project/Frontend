a0eb37cba890ad9f6c69f97ab024aa8f49217e19
kimyoungyin <yin199859@gmail.com>
2023-11-15 15:28:47 +0900

    issue #147 feat: change typing library which can stop erase

--- src/components/Landing/LandingMain/LandingMain.jsx
+++ src/components/Landing/LandingMain/LandingMain.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import ReactTypingEffect from "react-typing-effect";
+import { TypeAnimation } from "react-type-animation";
 
 import { motion } from "framer-motion";
 
@@ -8,18 +8,18 @@ import { SelodyLogoIcon } from "@/constants/iconConstants";
 
 import { LandingMainContainerDiv } from "./LandingMain.styles";
 
-const LandingMain = () => {
-	const bounceAnimation = {
-		scale: [1, 1.1, 1],
-		transition: {
-			scale: {
-				duration: 1,
-				repeat: Infinity,
-				ease: "easeInOut",
-			},
+const bounceAnimation = {
+	scale: [1, 1.1, 1],
+	transition: {
+		scale: {
+			duration: 1,
+			repeat: Infinity,
+			ease: "easeInOut",
 		},
-	};
+	},
+};
 
+const LandingMain = () => {
 	return (
 		<LandingMainContainerDiv>
 			<motion.div className="left" animate={bounceAnimation}>
@@ -33,23 +33,22 @@ const LandingMain = () => {
 				>
 					Selody와 함께 그룹 일정 공유 및 관리를 해보세요 !
 				</motion.h1>
-				<motion.div
-					className="typing-animation"
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					transition={{ delay: 1 }}
-				>
-					<ReactTypingEffect
-						text={[
-							"Selody는 개인 일정 관리 및 그룹 일정을 관리하는 플랫폼입니다. 프로젝트 공동 작업에서 이벤트 계획에 이르기까지 Selody는 일정을 단순화하여 더 쉽고 체계적으로 관리할 수 있습니다. 오늘 Selody와 함께 시작하세요!",
+				<div className="typing">
+					<TypeAnimation
+						sequence={[
+							"Selody는 개인 일정 관리 및 그룹 일정을 관리하는 플랫폼입니다.",
 						]}
-						speed={25}
-						eraseSpeed={30}
-						typingDelay={500}
-						eraseDelay={5000}
-						cursor=" "
+						wrapper="span"
+						cursor={true}
 					/>
-				</motion.div>
+					<TypeAnimation
+						sequence={[
+							"공동 작업에서 일정을 단순화히여 더 쉽고 체계적으로 관리합니다.",
+						]}
+						wrapper="span"
+						cursor={true}
+					/>
+				</div>
 				<motion.div
 					initial={{ opacity: 0, y: 50 }}
 					animate={{ opacity: 1, y: 0 }}
