84da721f6984e55c76111a6ecaa08bded8088632
2Junsu <ijh1205@naver.com>
2023-11-17 02:15:54 +0900

    issue #136 refactor: Refactor group service with commonThunk

--- src/features/group/group-service.js
+++ src/features/group/group-service.js
@@ -1,138 +1,112 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-import customFetch from "@/components/Base/BaseAxios.js";
+import commonThunk from "../commonThunk";
 
 export const createGroup = createAsyncThunk(
 	"group/createGroup",
 	async (groupName, thunkAPI) => {
-		try {
-			const response = await customFetch.post("/api/group", {
-				name: groupName,
-			});
-			if (response.status !== 200) {
-				throw response.data;
-			}
-			return response.data;
-		} catch (error) {
-			if (error.response) {
-				return thunkAPI.rejectWithValue(error.response.data);
-			}
-			return thunkAPI.rejectWithValue(error.message);
-		}
+		const response = await commonThunk(
+			{
+				method: "POST",
+				url: "/api/group",
+				data: { name: groupName },
+				successCode: 200,
+			},
+			thunkAPI,
+		);
+		return response;
 	},
 );
 
 export const getGroupList = createAsyncThunk(
 	"group/getGroupList",
 	async (_, thunkAPI) => {
-		try {
-			const response = await customFetch.get("/api/group");
-			if (response.status !== 200) {
-				throw response.data;
-			}
-			return response.data;
-		} catch (error) {
-			if (error.response) {
-				return thunkAPI.rejectWithValue(error.response.data);
-			}
-			return thunkAPI.rejectWithValue(error.message);
-		}
+		const response = await commonThunk(
+			{
+				method: "GET",
+				url: "/api/group",
+				successCode: 200,
+			},
+			thunkAPI,
+		);
+		return response;
 	},
 );
 
 export const deleteGroup = createAsyncThunk(
 	"group/deleteGroup",
 	async (groupId, thunkAPI) => {
-		try {
-			const response = await customFetch.delete(`/api/group/${groupId}`);
-
-			if (response.status !== 204) {
-				throw response.data;
-			}
-
-			return response;
-		} catch (error) {
-			return thunkAPI.rejectWithValue(error.response.data);
-		}
+		const response = await commonThunk(
+			{
+				method: "DELETE",
+				url: `/api/group/${groupId}`,
+				successCode: 204,
+			},
+			thunkAPI,
+		);
+		return response;
 	},
 );
 
 export const delegateGroup = createAsyncThunk(
 	"group/delegateGroup",
 	async ({ groupId, selectedMemberId }, thunkAPI) => {
-		try {
-			const response = await customFetch.patch(
-				`/api/group/${groupId}/members/${selectedMemberId}/access-level`,
-				{
-					access_level: "owner",
-				},
-			);
-
-			if (response.status !== 204) {
-				throw response.data;
-			}
-			return response;
-		} catch (error) {
-			return thunkAPI.rejectWithValue(error.response.data);
-		}
+		const response = await commonThunk(
+			{
+				method: "PATCH",
+				url: `/api/group/${groupId}/members/${selectedMemberId}/access-level`,
+				data: { access_level: "owner" },
+				successCode: 204,
+			},
+			thunkAPI,
+		);
+		return response;
 	},
 );
 
 export const updateGroup = createAsyncThunk(
 	"group/updateGroup",
 	async (groupId, groupLeader, thunkAPI) => {
-		try {
-			const response = await customFetch.patch(`/api/group/${groupId}`, {
-				newLeaderId: groupLeader,
-			});
-			if (response.status !== 200) {
-				throw response.data;
-			}
-			return response.data;
-		} catch (error) {
-			if (error.response) {
-				return thunkAPI.rejectWithValue(error.response.data);
-			}
-			return thunkAPI.rejectWithValue(error.message);
-		}
+		const response = await commonThunk(
+			{
+				method: "PATCH",
+				url: `/api/group/${groupId}`,
+				data: { newLeaderId: groupLeader },
+				successCode: 200,
+			},
+			thunkAPI,
+		);
+		return response;
 	},
 );
 
 export const leaveGroup = createAsyncThunk(
 	"group/leaveGroup",
 	async (groupId, thunkAPI) => {
-		try {
-			const response = await customFetch.delete(`/api/user/group/${groupId}`);
-			if (response.status !== 204) {
-				throw response.data;
-			}
-			return response;
-		} catch (error) {
-			if (error.response) {
-				return thunkAPI.rejectWithValue(error.response.data);
-			}
-			return thunkAPI.rejectWithValue(error.message);
-		}
+		const response = await commonThunk(
+			{
+				method: "DELETE",
+				url: `/api/user/group/${groupId}`,
+				successCode: 204,
+			},
+			thunkAPI,
+		);
+		return response;
 	},
 );
 
 export const changeGroupOption = createAsyncThunk(
 	"group/changeOption",
 	async ({ groupId, type, status }, thunkAPI) => {
-		try {
-			const response = await customFetch.patch(
-				`/api/user/settings/${groupId}`,
-				{
-					[type]: status,
-				},
-			);
-
-			if (response.status !== 200) {
-				throw response.data;
-			}
-			return response.status;
-		} catch (error) {
-			return thunkAPI.rejectWithValue(error.response.data);
-		}
+		const response = await commonThunk(
+			{
+				method: "PATCH",
+				url: `/api/user/settings/${groupId}`,
+				data: { [type]: status },
+				successCode: 200,
+			},
+			thunkAPI,
+		);
+		return response;
 	},
 );
