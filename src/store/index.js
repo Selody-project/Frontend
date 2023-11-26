4a50f14465d3fe3b5eef5a283fa9957f53ccd767
sikkzz <sling0623@gmail.com>
2023-11-26 15:18:42 +0900

    issue #117 refactor: conflict resolve

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,6 @@ import groupInviteSlice from "@/features/group/group-invite-slice.js";
 import groupSlice from "@/features/group/group-slice.js";
 import scheduleSlice from "@/features/schedule/schedule-slice.js";
 import uiSlice from "@/features/ui/ui-slice.js";
-import userSlice from "@/features/user/user-slice";
 
 const rootReducer = combineReducers({
 	auth: authSlice,
@@ -13,7 +12,6 @@ const rootReducer = combineReducers({
 	group: groupSlice,
 	groupInvite: groupInviteSlice,
 	ui: uiSlice,
-	user: userSlice,
 });
 
 export const setupStore = (preloadedState) => {
