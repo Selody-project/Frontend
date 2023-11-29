916651f83e4da413154da47248c467ba4525a442
kimyoungyin <yin199859@gmail.com>
2023-11-29 17:32:26 +0900

    issue #157 fix: fix commonThunk catch statement for showing error message in toast

--- src/features/commonThunk.js
+++ src/features/commonThunk.js
@@ -20,7 +20,7 @@ const commonThunk = async (
 		if (error.response) {
 			return thunkAPI.rejectWithValue(error.response.data);
 		}
-		return thunkAPI.rejectWithValue(error.message);
+		return thunkAPI.rejectWithValue(error);
 	}
 };
 
