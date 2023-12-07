1311b712362df89851364876914920341dcc56e9
sikkzz <sling0623@gmail.com>
2023-12-07 15:36:01 +0900

    issue #138 refactor: group api conflict resolve

--- src/components/Community/GroupSearch/GroupSearch.jsx
+++ src/components/Community/GroupSearch/GroupSearch.jsx
@@ -1,27 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 import SampleImg from "@/assets/img/feed/img-group-sample-01.jpeg";
+import { getGroupList } from "@/features/group/group-service";
 
 import { ContainerDiv, GroupDiv } from "./GroupSearch.styles";
 
 const GroupSearch = () => {
+	const dispatch = useDispatch();
+
+	const groupList = useSelector((state) => state.group.groupList);
+
+	useEffect(() => {
+		dispatch(getGroupList(1));
+	}, []);
+
 	return (
 		<ContainerDiv>
-			<GroupDiv>
-				<img src={SampleImg} alt="sampleimg" />
-				<h3>CodeCrafters</h3>
-				<p>
-					당신의 개발 열정을 키우고
-					<br />
-					함께 성장할 수 있는 공간, CodeCrafters
-				</p>
-				<h4>21명의 그룹원</h4>
-			</GroupDiv>
-			<GroupDiv />
-			<GroupDiv />
-			<GroupDiv />
-			<GroupDiv />
-			<GroupDiv />
+			{groupList?.map((info) => (
+				<GroupDiv key={info.groupId}>
+					<img src={SampleImg} alt="sampleimg" />
+					<h3>{info.name}</h3>
+					<p>{info.description}</p>
+					<h4>{info.member}명의 그룹원</h4>
+				</GroupDiv>
+			))}
 		</ContainerDiv>
 	);
 };
