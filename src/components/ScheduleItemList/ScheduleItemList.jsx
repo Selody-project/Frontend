36f4a2aa82232e53899997d8ebff739cce66bf2c
kimyoungyin <yin199859@gmail.com>
2023-12-15 13:41:32 +0900

    issue #129 refactor: change ScheduleModal render position to parent PersonalSchedulePage

--- src/components/ScheduleItemList/ScheduleItemList.jsx
+++ src/components/ScheduleItemList/ScheduleItemList.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import ScheduleModal from "@/components/Common/ScheduleModal/ScheduleModal.jsx";
 import ScheduleItem from "@/components/ScheduleItemList/ScheduleItem/ScheduleItem";
 import { ScheduleAddIcon } from "@/constants/iconConstants";
 import { UI_TYPE } from "@/constants/uiConstants";
@@ -26,7 +25,6 @@ import {
 
 const ScheduleItemList = () => {
 	const dispatch = useDispatch();
-	const { openedModal } = useSelector((state) => state.ui);
 	const { todaySchedules, schedulesForTheWeek } = useSelector(
 		(state) => state.schedule,
 	);
@@ -46,61 +44,56 @@ const ScheduleItemList = () => {
 	};
 
 	return (
-		<>
-			<ScheduleItemListLayoutAside data-testid="personal-todo-list">
-				<TodoHeader>
-					<TodoTab selected={isTodayTab} onClick={() => setIsTodayTab(true)}>
-						오늘 일정
-					</TodoTab>
-					<TodoTab selected={!isTodayTab} onClick={() => setIsTodayTab(false)}>
-						예정
-					</TodoTab>
-				</TodoHeader>
-				<TodoBody>
-					<TodoBodyHeader>
-						<div>
-							<TodoH2>{isTodayTab ? "오늘 일정" : "예정"}</TodoH2>
-							<TodoH3>
-								{isTodayTab
-									? "하루동안의 할 일을 관리합니다."
-									: "앞으로 7일간 예정된 일정을 확인합니다."}
-							</TodoH3>
-						</div>
-						<ScheduleAddButton onClick={handleMenuOpen}>
-							<ScheduleAddIcon />
-							<span>일정 추가</span>
-						</ScheduleAddButton>
-					</TodoBodyHeader>
-					{(isTodayTab && todaySchedules.length === 0) ||
-					(!isTodayTab && schedulesForTheWeek.length === 0) ? (
-						<TodoButton onClick={handleMenuOpen}>
-							아직 추가된 일정이 없습니다! <br />할 일을 추가하여 하루동안 할
-							일을 관리해보세요.
-						</TodoButton>
-					) : (
-						<TodoList>
-							{(isTodayTab ? todaySchedules : schedulesForTheWeek).map(
-								(schedule) => {
-									return (
-										<ScheduleItem
-											key={
-												schedule.recurrence
-													? schedule.startDateTime + schedule.id
-													: schedule.id
-											}
-											schedule={schedule}
-										/>
-									);
-								},
-							)}
-						</TodoList>
-					)}
-				</TodoBody>
-			</ScheduleItemListLayoutAside>
-			{openedModal === UI_TYPE.PERSONAL_SCHEDULE && (
-				<ScheduleModal type={openedModal} />
-			)}
-		</>
+		<ScheduleItemListLayoutAside data-testid="personal-todo-list">
+			<TodoHeader>
+				<TodoTab selected={isTodayTab} onClick={() => setIsTodayTab(true)}>
+					오늘 일정
+				</TodoTab>
+				<TodoTab selected={!isTodayTab} onClick={() => setIsTodayTab(false)}>
+					예정
+				</TodoTab>
+			</TodoHeader>
+			<TodoBody>
+				<TodoBodyHeader>
+					<div>
+						<TodoH2>{isTodayTab ? "오늘 일정" : "예정"}</TodoH2>
+						<TodoH3>
+							{isTodayTab
+								? "하루동안의 할 일을 관리합니다."
+								: "앞으로 7일간 예정된 일정을 확인합니다."}
+						</TodoH3>
+					</div>
+					<ScheduleAddButton onClick={handleMenuOpen}>
+						<ScheduleAddIcon />
+						<span>일정 추가</span>
+					</ScheduleAddButton>
+				</TodoBodyHeader>
+				{(isTodayTab && todaySchedules.length === 0) ||
+				(!isTodayTab && schedulesForTheWeek.length === 0) ? (
+					<TodoButton onClick={handleMenuOpen}>
+						아직 추가된 일정이 없습니다! <br />할 일을 추가하여 하루동안 할 일을
+						관리해보세요.
+					</TodoButton>
+				) : (
+					<TodoList>
+						{(isTodayTab ? todaySchedules : schedulesForTheWeek).map(
+							(schedule) => {
+								return (
+									<ScheduleItem
+										key={
+											schedule.recurrence
+												? schedule.startDateTime + schedule.id
+												: schedule.id
+										}
+										schedule={schedule}
+									/>
+								);
+							},
+						)}
+					</TodoList>
+				)}
+			</TodoBody>
+		</ScheduleItemListLayoutAside>
 	);
 };
 
--- src/pages/PersonalSchedulePage/PersonalSchedulePage.jsx
+++ src/pages/PersonalSchedulePage/PersonalSchedulePage.jsx
@@ -2,21 +2,29 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 import CalendarContainer from "@/components/Common/CalendarContainer/CalendarContainer";
+import ScheduleModal from "@/components/Common/ScheduleModal/ScheduleModal";
 import ScheduleItemList from "@/components/ScheduleItemList/ScheduleItemList";
 import { SCHEDULE_TYPE, VIEW_TYPE } from "@/constants/calendarConstants";
+import { UI_TYPE } from "@/constants/uiConstants";
 
 import { LayoutMain } from "./PersonalSchedulePage.styles";
 
 const PersonalSchedulePage = () => {
+	const openedModal = useSelector(({ ui }) => ui.openedModal);
 	const currentCalendarView = useSelector(
 		({ schedule }) => schedule.currentCalendarView,
 	);
 
 	return (
-		<LayoutMain isMonthly={currentCalendarView === VIEW_TYPE.DAY_GRID_MONTH}>
-			<CalendarContainer type={SCHEDULE_TYPE.PERSONAL} />
-			<ScheduleItemList />
-		</LayoutMain>
+		<>
+			<LayoutMain isMonthly={currentCalendarView === VIEW_TYPE.DAY_GRID_MONTH}>
+				<CalendarContainer type={SCHEDULE_TYPE.PERSONAL} />
+				<ScheduleItemList />
+			</LayoutMain>
+			{openedModal === UI_TYPE.PERSONAL_SCHEDULE && (
+				<ScheduleModal type={openedModal} />
+			)}
+		</>
 	);
 };
 
