3e335b0b888635f6905edd99b09ec092f61671d5
kimyoungyin <yin199859@gmail.com>
2024-01-03 17:58:01 +0900

    issue #129 test: intercepting network call during initial rendering PersonalSchedule with msw handlers

--- __test__/__mocks__/msw/handlers.js
+++ __test__/__mocks__/msw/handlers.js
@@ -3,50 +3,62 @@ import { rest } from "msw";
 const BASE_URL = "http://localhost/back";
 
 export const handlers = [
-	rest.post(`${BASE_URL}/api/user/calendar`, (req, res, ctx) => {
-		return res(
-			ctx.status(201),
-			ctx.json({ message: "Successfully create user schedule" }),
-		);
-	}),
-	rest.get(`${BASE_URL}/api/user/calendar`, (req, res, ctx) => {
+	rest.get(`${BASE_URL}/api/user/calendar/summary`, (req, res, ctx) => {
+		const startDateTime = req.url.searchParams.get("startDateTime");
+		const endDateTime = new Date(
+			new Date(startDateTime).setHours(23, 59, 59, 999),
+		).toISOString();
 		return res(
 			ctx.status(200),
 			ctx.json({
-				recurrenceSchedule: [
-					{
-						byweekday: "",
-						content: "test-content11",
-						freq: "DAILY",
-						groupId: 1,
-						id: 11,
-						interval: 1,
-						recurrence: 1,
-						recurrenceDateList: [
-							{
-								endDateTime: "2023-04-01T13:00:00.000Z",
-								startDateTime: "2023-04-01T12:00:00.000Z",
-							},
-							{
-								endDateTime: "2023-04-02T13:00:00.000Z",
-								startDateTime: "2023-04-02T12:00:00.000Z",
-							},
-						],
-						title: "test-title11",
-						until: "2023-04-05T14:00:00.000Z",
-					},
-				],
-				nonRecurrenceSchedule: [
+				schedules: [
 					{
 						id: 1,
-						content: "test-content1",
-						endDateTime: "2023-05-15T23:59:59.000Z",
+						userId: 1,
+						startDateTime,
+						endDateTime,
 						recurrence: 0,
-						startDateTime: "2023-02-03T00:00:00.000Z",
-						title: "test-title1",
+						freq: null,
+						interval: null,
+						byweekday: null,
+						until: null,
+						isGroup: 0,
 					},
 				],
 			}),
 		);
 	}),
+	rest.get(`${BASE_URL}/api/user/calendar`, (req, res, ctx) => {
+		const startDateTime = req.url.searchParams.get("startDateTime");
+		const endDateTime = new Date(
+			new Date(startDateTime).setHours(23, 59, 59, 999),
+		).toISOString();
+		return res(
+			ctx.status(200),
+			ctx.json({
+				schedules: [
+					{
+						id: 1,
+						userId: 1,
+						title: "오늘오늘",
+						content: "오늘 끝",
+						startDateTime,
+						endDateTime,
+						recurrence: 0,
+						freq: null,
+						interval: null,
+						byweekday: null,
+						until: null,
+						isGroup: 0,
+					},
+				],
+			}),
+		);
+	}),
+	// rest.post(`${BASE_URL}/api/user/calendar`, (req, res, ctx) => {
+	// 	return res(
+	// 		ctx.status(201),
+	// 		ctx.json({ message: "Successfully create user schedule" }),
+	// 	);
+	// }),
 ];
