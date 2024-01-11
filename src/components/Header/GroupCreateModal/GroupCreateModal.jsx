2e1af7f9ab23f406c762f8ee026300d3bc2d8820
kimyoungyin <yin199859@gmail.com>
2024-01-11 15:14:34 +0900

    issue #176 refactor: use FormModal to GroupCreateModal

--- src/components/Header/GroupCreateModal/GroupCreateModal.jsx
+++ src/components/Header/GroupCreateModal/GroupCreateModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import BaseModal from "@/components/Common/Modal/BaseModal.jsx";
+import FormModal from "@/components/Common/Modal/FormModal/FormModal.jsx";
 
 import {
 	ButtonWrapDiv,
@@ -11,27 +11,39 @@ import {
 
 const GroupCreateModal = () => {
 	const [name, setName] = useState("");
-	// const [introduction, setIntroduction] = useState("");
+	const [description, setDescription] = useState("");
 
-	const handleClick = () => {};
+	const handleCreateGroup = (event) => {
+		event.preventDefault();
+		// 업로드하는 로직
+		// ...
+	};
+
+	const isEmpty = name.trim() === "" && description.trim() === "";
 
 	return (
-		<BaseModal>
+		<FormModal isEmpty={isEmpty}>
 			{/* <BaseModal title="그룹 만들기" bg="#fff"> */}
 			<GroupNameInput
 				placeholder="그룹 이름"
 				onChange={(e) => setName(e.target.value)}
+				value={name}
 			/>
 			<GroupIntroduceTextarea
 				placeholder="그룹 소개"
-				// onChange={(e) => setIntroduction(e.target.value)}
+				onChange={(e) => setDescription(e.target.value)}
+				value={description}
 			/>
 			<ButtonWrapDiv>
-				<GroupCreateButton disabled={name.trim() === ""} onClick={handleClick}>
+				<GroupCreateButton
+					type="submit"
+					disabled={!name.trim()}
+					onSubmit={handleCreateGroup}
+				>
 					생성하기
 				</GroupCreateButton>
 			</ButtonWrapDiv>
-		</BaseModal>
+		</FormModal>
 	);
 };
 
