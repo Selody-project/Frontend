53a3fd04c0bf3c188c33d30203c57e152ccc63a9
sikkzz <sling0623@gmail.com>
2024-01-13 19:25:55 +0900

    issue #138 refactor: group create modal refactoring

--- src/components/Header/GroupCreateModal/GroupCreateModal.jsx
+++ src/components/Header/GroupCreateModal/GroupCreateModal.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 
 import FormModal from "@/components/Common/Modal/FormModal/FormModal.jsx";
+import { GroupImgAddIcon } from "@/constants/iconConstants.js";
 import { createGroup } from "@/features/group/group-service.js";
 import { closeModal } from "@/features/ui/ui-slice.js";
 
@@ -17,32 +18,47 @@ import {
 } from "./GroupCreateModal.style.js";
 
 const GroupCreateModal = () => {
-	const dispatch = useDispatch();
-
 	const [name, setName] = useState("");
 	const [description, setDescription] = useState("");
 
-	const handleCreateGroup = (event) => {
-		event.preventDefault();
-		dispatch(createGroup({ name, description }));
-		dispatch(closeModal({ type: "CREATE_GROUP" }));
-	};
+	const dispatch = useDispatch();
 
 	const isEmpty = name.trim() === "" && description.trim() === "";
+
+	const handleCreateGroup = (event) => {
+		event.preventDefault();
+
+		const formData = new FormData();
+
+		const data = {
+			name,
+			description: description.trim(),
+		};
+
+		formData.append("data", JSON.stringify(data));
+
+		if (name.length < 21 && description.length < 101) {
+			dispatch(createGroup(formData));
+			dispatch(closeModal({ type: "CREATE_GROUP" }));
+		}
+	};
 
 	return (
 		<FormModal isEmpty={isEmpty}>
 			<TopDiv>
 				<TitleH2>그룹 만들기</TitleH2>
+				<GroupImgAddIcon />
 			</TopDiv>
 			<GroupNameH3>그룹 이름</GroupNameH3>
 			<GroupNameTextarea
 				onChange={(e) => setName(e.target.value)}
+				value={name}
 				maxLength={20}
 			/>
 			<GroupIntroduceH3>그룹 소개</GroupIntroduceH3>
 			<GroupIntroduceTextarea
 				onChange={(e) => setDescription(e.target.value)}
+				value={description}
 				maxLength={100}
 			/>
 			<ButtonWrapDiv>
